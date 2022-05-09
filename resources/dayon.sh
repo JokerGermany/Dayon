94b39245e13f1d99129dc1c6f55130cbda7bbb34
retgal <galante@puzzle.ch>
2022-05-09 08:00:36 +0200

    Fixed shell script for flatpak
    (cherry picked from commit 0205c5ec59b23b45d8b7ad5adf601e2f43a0a2ed)

--- resources/dayon.sh
+++ resources/dayon.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 DAYON_HOME=$(dirname $(realpath "$0"))
 if which java >/dev/null; then
-    JAVA=$(which java)
-  else
-    JAVA_HOME=`ls -l /etc/alternatives/java | awk -F'> ' '{print $2}' | awk -F'/bin/java' '{print $1}'`
-    #JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/jre"
-    JAVA=${JAVA_HOME}/bin/java
+  JAVA=$(which java)
+elif [ ! -f /etc/alternatives/java ]; then
+  JAVA=/app/jre/bin/java
+else
+  JAVA_HOME=`ls -l /etc/alternatives/java | awk -F'> ' '{print $2}' | awk -F'/bin/java' '{print $1}'`
+  #JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/jre"
+  JAVA=${JAVA_HOME}/bin/java
 fi
 JAVA_OPTS=""
 CLASSPATH="$DAYON_HOME/dayon.jar"
